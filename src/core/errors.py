0a03b9a47d562f642ce040ad3fca3a147d591f79
dmitriy levykin <dmlegasy@gmail.com>
2026-04-25 11:11:32 +0300

    feat(security): add csrf exception handle

---
+++ src/core/errors.py
@@ -0,0 +1,15 @@
+from fastapi import FastAPI, Request
+from fastapi.responses import JSONResponse
+from fastapi_csrf_protect.exceptions import CsrfProtectError
+
+from src.core.logging_app import get_logger
+
+logger = get_logger(__name__)
+
+async def _csrf_protect_exception_handler(request: Request, exc: CsrfProtectError) -> JSONResponse:
+    logger.error(exc.message)
+    return JSONResponse(status_code=exc.status_code, content={"detail": "Error with CSRF protection"})
+
+
+def setup_errors(app: FastAPI) -> None:
+    app.add_exception_handler(CsrfProtectError, _csrf_protect_exception_handler)
