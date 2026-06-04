7f3ee23e58cff011f1738aaf746cc702b47690a9
dmitriy levykin <dmlegasy@gmail.com>
2026-06-04 22:05:37 +0300

    test: add tests for auth repository

---
+++ tests/auth/test_repository.py
@@ -0,0 +1,72 @@
+import pytest
+
+from src.auth.models import Session, User
+from tests.factory import make_credentials, make_session_key
+
+
+@pytest.mark.parametrize(
+    "credentials",
+    [
+        (make_credentials()),
+        (make_credentials()),
+        (make_credentials()),
+    ],
+)
+async def test_create_user(user_repo, credentials):
+    user = await user_repo.create_user(credentials)
+
+    assert user is not None
+    assert isinstance(user, User)
+    assert isinstance(user.username, str)
+
+
+@pytest.mark.parametrize(
+    ["credentials", "session_key"],
+    [
+        (make_credentials(), make_session_key()),
+        (make_credentials(), make_session_key()),
+        (make_credentials(), make_session_key()),
+    ],
+)
+async def test_create_session(user_repo, session_repo, credentials, session_key):
+    user = await user_repo.create_user(credentials)
+    session = await session_repo.create(session_key, user.username, user)
+
+    assert user is not None
+    assert isinstance(user, User)
+
+    assert session is not None
+    assert isinstance(session, Session)
+
+
+@pytest.mark.parametrize(
+    "credentials",
+    [
+        make_credentials(),
+        make_credentials(),
+        make_credentials(),
+    ],
+)
+async def test_get_user_by_email(user_repo, credentials):
+    await user_repo.create_user(credentials)
+    user = await user_repo.get_user_by_email(credentials.email)
+
+    assert user is not None
+    assert isinstance(user, User)
+
+
+@pytest.mark.parametrize(
+    ["credentials", "session_key"],
+    [
+        (make_credentials(), make_session_key()),
+        (make_credentials(), make_session_key()),
+        (make_credentials(), make_session_key()),
+    ],
+)
+async def test_delete_session(user_repo, session_repo, credentials, session_key):
+    user = await user_repo.create_user(credentials)
+    await session_repo.create(session_key, user.username, user)
+
+    result = await session_repo.delete(session_key)
+
+    assert isinstance(result, bool)
